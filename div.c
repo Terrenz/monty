04225a3f437be61b4263daff66303d2be47e4487
Terrence Peters <terrencepeters54@gmail.com>
2022-06-16 14:51:59 -0700

    div.c

--- div.c
+++ div.c
@@ -16,7 +16,7 @@ void _div(stack_t **head, unsigned int line_number)
 	{
 		fprintf(stderr, "L%u: can't div, stack too short\n", line_number);
 		free_stack_t(*head);
-		
+
 		exit(EXIT_FAILURE);
 	}
 
